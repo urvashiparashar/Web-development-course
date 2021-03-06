a889424afdb3eceb26a1b2e108598c2201dc93b0
Urvashi Parashar <urvashiparashar2001@gmail.com>
2021-03-06 11:15:50 +0530

    changes made

---
+++ javascript/basicjs.js
@@ -0,0 +1,4 @@
+alert("welcome to verzeo");
+var course=prompt("which course you like to register?");
+alert("you have successfully registered in "+course);
+console.log("Congratulations you have registered course: ");
