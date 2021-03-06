262fe9f9e87706232eb5d88e244fdee8508e119b
Urvashi Parashar <urvashiparashar2001@gmail.com>
2021-03-06 17:09:56 +0530

    added changes

---
+++ javascript/comparisionjs.js
@@ -0,0 +1,22 @@
+/*console.log(5>1);
+console.log(3>8);
+console.log(4<7);
+console.log(1<=1);
+console.log(2<=8);
+console.log(9==9);
+console.log("employee"=="employee");
+console.log("10"==10);
+console.log(5!=="5");
+console.log(2>-2);
+console.log("11"===11);
+console.log("5"!=5);
+*/
+/*console.log("5"!=5);*/
+// console.log(true==1); ///value
+// console.log(true===1); ///value and data type
+// console.log(false==0); 
+// console.log(false===0);
+// console.log(null==undefined);
+// console.log(null===undefined);
+console.log(NaN==NaN);
+console.log(NaN===NaN);
