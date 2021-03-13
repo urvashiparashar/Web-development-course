4735b0362fa0d3e4240c16d4ead34f0925b4fb84
Urvashi Parashar <urvashiparashar2001@gmail.com>
2021-03-14 00:01:55 +0530

    added

---
+++ jquery-intro/jquery_intro.js
@@ -0,0 +1,13 @@
+// var list_items=document.querySelectorAll('li');
+// var list_items=$('li');
+// window.addEventListener("load",()=>
+// {
+//     alert("hello world");
+//     document.getElementById("blackbox").style.display="none";
+// });
+
+//using jquery
+(document).ready(function(){
+    alert("hello world|||");
+    $("#blackbox").hide();
+});
