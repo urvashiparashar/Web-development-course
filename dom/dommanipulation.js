a0c12615992cb97a7d5b6d6df601c560125af0bd
Urvashi Parashar <urvashiparashar2001@gmail.com>
2021-03-13 23:02:50 +0530

    added more

---
+++ dom/dommanipulation.js
@@ -0,0 +1,7 @@
+var oldparagraph=document.querySelector('p');
+oldparagraph.textContent="all hello";
+"all hello"
+oldparagraph.textContent="<em>all hello</em>";
+"<em>all hello</em>"
+oldparagraph.innerHTML="<em>all hello</em>";
+"<em>all hello</em>"
